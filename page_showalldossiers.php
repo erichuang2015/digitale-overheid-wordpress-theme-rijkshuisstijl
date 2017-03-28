2b11b4f63c338aadcffc0092366c229ba7b35320
Paul van Buuren <paul@wbvb.nl>
2017-03-28 17:02:59 +0200

    dossierpagina aangepast
    Meer mogelijkheden voor het tonen van de dossiers

--- page_showalldossiers.php
+++ page_showalldossiers.php
@@ -23,9 +23,6 @@ add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_c
 $wrapper_title  = '';
 $checker        = '';
 
-$wrapper_start  = '<div class="block no-top">';
-$wrapper_end    = '</div>';
-
 
 add_action( 'genesis_entry_content', 'rhswp_show_all_dossiers', 15 );
 
@@ -42,71 +39,139 @@ genesis();
 
 function rhswp_show_all_dossiers() {
   global $post;
-  global $wrapper_start;
-  global $wrapper_end;
-
-  $title          = get_field('dossier_overzicht_filter_title', $post->ID );
-  $dossierfilter  = get_field('dossier_overzicht_filter', $post->ID );
-  $grootte        = get_field('uitgelichte_dossiers', $post->ID );
 
 
-  if ( ( $grootte ) &&  ( 'dossier_overzicht_filter_ongefilterd' != $dossierfilter ) ) {
-    $args = array(
-      'taxonomy'              => RHSWP_CT_DOSSIER,
-      'hide_empty'            => false,
-      'include'               => $grootte,
-      'orderby'               => 'name',
-      'order'                 => 'ASC',
-      'ignore_custom_sort'    => TRUE,
-      'echo'                  => 0,
-      'title_li'              => ''
-    );
-  }
-  else {
-    $args = array(
-      'taxonomy'              => RHSWP_CT_DOSSIER,
-      'hide_empty'            => false,
-      'orderby'               => 'name',
-      'order'                 => 'ASC',
-      'ignore_custom_sort'    => TRUE,
-      'echo'                  => 0,
-      'title_li'              => ''
-    );
+  $title          = '';
+  $dossierfilter  = '';
+  $grootte        = '';
+
+  if ( function_exists( 'get_field' ) ) {
+	  $title          = get_field('dossier_overzicht_filter_title', $post->ID );
+	  $dossierfilter  = get_field('dossier_overzicht_filter', $post->ID );
+	  $grootte        = get_field('uitgelichte_dossiers', $post->ID );
   }
 
-//  $terms = wp_list_categories( $args );
+// 1 toon alles
+// 2 toon alles en uitgelichte dossiers
+// 3 toon alleen uitgelichte dossiers
+
+  $args = array(
+    'taxonomy'              => RHSWP_CT_DOSSIER,
+    'hide_empty'            => false,
+    'orderby'               => 'name',
+    'order'                 => 'ASC',
+    'ignore_custom_sort'    => TRUE,
+    'echo'                  => 0,
+    'title_li'              => ''
+  );
+  
+	echo '<div class="topicSearchWrapper"><form tabindex="-1" id="rhswp-searchform" class="search-form" itemprop="potentialAction" itemscope="" itemtype="http://schema.org/SearchAction" method="get" action="/" role="search"><meta itemprop="target" content="/?s={s}"><label class="search-form-label screen-reader-text" for="searchform-58da65bb69ca2">Zoek een onderwerp</label><input itemprop="query-input" type="search" name="s" id="searchform-58da65bb69ca2" placeholder="Zoek een onderwerp"><input type="submit" value="Zoek"></form></div>';  
+	  
+	
+  if ( ( $grootte ) &&  ( ( 'dossier_overzicht_filter_plus' == $dossierfilter ) || ( 'dossier_overzicht_filter_only_filter' == $dossierfilter ) ) ) {
+
+	  if ( 'dossier_overzicht_filter_plus' == $dossierfilter ) {
+
+	    $args_filter = array(
+	      'taxonomy'              => RHSWP_CT_DOSSIER,
+	      'hide_empty'            => false,
+	      'include'               => $grootte,
+	      'orderby'               => 'name',
+	      'order'                 => 'ASC',
+	      'ignore_custom_sort'    => TRUE,
+	      'echo'                  => 0,
+	      'title_li'              => ''
+	    );
+
+		  $terms = get_terms( RHSWP_CT_DOSSIER, $args_filter );
+		
+		  if ($terms && ! is_wp_error( $terms ) ) { 
+		
+	      echo '<div class="block no-top dossier_overzicht_popular ' . $dossierfilter . '">';
+	      echo '<h2>' . $title . '</h2>';
+	      echo '<ul class="links">';
+	  
+	      foreach ( $terms as $term ) {
+	  
+	        $excerpt    = '';
+	        $classattr  = 'class="dossieroverzicht"';
+	        $href       = get_term_link( $term->term_id, RHSWP_CT_DOSSIER );
+	        
+	        printf( '<li><a href="%s">%s</a>', $href, $term->name );
+	
+	      }
+	
+	      echo '</ul>';
+	      echo '</div>';
+		    
+		  }
+		  
+	  }
+	  elseif ( 'dossier_overzicht_filter_only_filter' == $dossierfilter ) {
+	    $args = array(
+	      'taxonomy'              => RHSWP_CT_DOSSIER,
+	      'hide_empty'            => false,
+	      'include'               => $grootte,
+	      'orderby'               => 'name',
+	      'order'                 => 'ASC',
+	      'ignore_custom_sort'    => TRUE,
+	      'echo'                  => 0,
+	      'title_li'              => ''
+	    );
+	  }
+  }
 
   $terms = get_terms( RHSWP_CT_DOSSIER, $args );
 
-  if ($terms && ! is_wp_error( $terms ) ) { 
+	if ($terms && ! is_wp_error( $terms ) ) { 
+		
+    echo '<div class="block no-top ' . $dossierfilter . '">';
 
-    echo $wrapper_start;
+	  if ( 'dossier_overzicht_filter_as_list' == $dossierfilter ) {
+			echo '<ul class="links">';
+	  }
+		
+		foreach ( $terms as $term ) {
 
+			$href       = get_term_link( $term->term_id, RHSWP_CT_DOSSIER );
 
-      echo '<div class="block no-top">';
-  
-      foreach ( $terms as $term ) {
-  
-        $excerpt    = '';
-        $classattr  = 'class="dossieroverzicht"';
-        if ( $term->description ) {
-          $excerpt  =  wp_strip_all_tags( $term->description );
-        }
-        
-        $href       = get_term_link( $term->term_id, RHSWP_CT_DOSSIER );
-        
-        printf( '<article %s>', $classattr );
-        printf( '<a href="%s"><h2>%s</h2><p>%s</p></a>', $href, $term->name, $excerpt );
-        echo '</article>';
+			if ( 'dossier_overzicht_filter_as_list' == $dossierfilter ) {
+				printf( '<li><a href="%s">%s</a></li>', $href, $term->name );
+			}
+			else {
+			
+				$excerpt    	= '';
+				$classattr  	= 'class="dossieroverzicht"';
+				$kortebeschr	= get_field( 'dossier_korte_beschrijving_voor_dossieroverzicht', RHSWP_CT_DOSSIER . '_' . $term->term_id );
+				
+				if ( $kortebeschr ) {
+					$excerpt  =  wp_strip_all_tags( $kortebeschr );
+				}        
+				elseif ( $term->description ) {
+					$excerpt  =  wp_strip_all_tags( $term->description );
+					$excerptarr = explode( '.', $excerpt );
+					$excerpt = $excerptarr[0] . '.';
+				}
+				
+				
+				printf( '<article %s>', $classattr );
+				printf( '<a href="%s"><h2>%s</h2><p>%s</p></a>', $href, $term->name, $excerpt );
+				echo '</article>';
+			
+			}			
+			
+		}
+		
+	  if ( 'dossier_overzicht_filter_as_list' == $dossierfilter ) {
+			echo '</ul>';
+	  }
 
-      }
+		echo '</div>';
+		
+		wp_reset_postdata();
+		
 
-      echo '</div>';
-    
-    wp_reset_postdata();
-
-    echo $wrapper_end;
-  }
+	}
 }
 
 
