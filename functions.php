beeb9b2ac0c7c1bc3f61835b2602c966c5663f76
Paul van Buuren <paul@wbvb.nl>
2017-03-28 17:01:28 +0200

    Walker
    Walker toegevoegd voor categories / dossiers met children

--- functions.php
+++ functions.php
@@ -1029,6 +1029,51 @@ function rhswp_get_sitemap_content() {
 
 //========================================================================================================
 
+class rhswp_custom_walker_for_taxonomies extends Walker_Category {
+  function start_el( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
+
+    extract($args);
+    
+    $cat_name = esc_attr( $category->name );
+    $cat_name = apply_filters( 'list_cats', $cat_name, $category );
+    $link = '<a href="' . esc_url( get_term_link($category) ) . '" ';
+    if ( $use_desc_for_title == 0 || empty($category->description) )
+      $link .= 'title="' . esc_attr( sprintf(__( 'View all posts filed under %s' ), $cat_name) ) . '"';
+    else
+      $link .= 'title="' . esc_attr( strip_tags( apply_filters( 'category_description', $category->description, $category ) ) ) . '"';
+      $link .= '>';
+      $link .= $cat_name . '</a>';
+    
+    if ( !empty($show_count) )
+      $link .= ' (' . intval($category->count) . ')';
+    
+      if ( 'list' == $args['style'] ) {
+        $output .= "\t<li";
+        $class = 'cat-item cat-item-' . $category->term_id;
+        
+        $termchildren = get_term_children( $category->term_id, $category->taxonomy );
+        if(count($termchildren)>0){
+          $class .=  ' i-have-kids';
+        }
+        
+        if ( !empty($current_category) ) {
+          $_current_category = get_term( $current_category, $category->taxonomy );
+          if ( $category->term_id == $current_category )
+            $class .=  ' current-cat';
+          elseif ( $category->term_id == $_current_category->parent )
+            $class .=  ' current-cat-parent';
+        }
+        
+        $output .=  ' class="' . $class . '"';
+        $output .= ">$link\n";
+      }
+      else {
+        $output .= "\t$link<br />\n";
+      }
+  }
+}
+//========================================================================================================
+
 class rhswp_custom_walker_for_sitemap extends Walker_Page {
 
   // -------------------------
@@ -1670,8 +1715,9 @@ function rhswp_show_customtax_terms( $taxonomy, $title = '', $dosection = true )
       'hide_empty'            => true,      
       'ignore_custom_sort'    => TRUE,
       'echo'                  => 0,
-      'title_li'              => ''
-
+      'hierarchical'          => TRUE,
+      'title_li'              => '',
+      'walker'                => new rhswp_custom_walker_for_taxonomies()
     );
 
     $terms = wp_list_categories( $args );
