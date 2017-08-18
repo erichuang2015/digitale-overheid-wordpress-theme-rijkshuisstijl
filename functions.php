929acb08ed27d2a02a712cc2fbdd824161447f89
Paul van Buuren <paul@wbvb.nl>
2017-08-18 12:51:04 +0200

    0.10.1 - Div CSS styling issues: h2, external link.

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
  * @author  Paul van Buuren
  * @license GPL-2.0+
  * @package wp-rijkshuisstijl
- * @version 0.9.8
- * @desc.   CSS voor onderwerppagina.
+ * @version 0.10.1
+ * @desc.   Div CSS styling issues: h2, external link.
  * @link    https://github.com/ICTU/digitale-overheid-wordpress-theme-rijkshuisstijl
  */
 
@@ -23,8 +23,8 @@ include_once( get_template_directory() . '/lib/init.php' );
 // Constants
 define( 'CHILD_THEME_NAME',                 "Rijkshuisstijl (Digitale Overheid)" );
 define( 'CHILD_THEME_URL',                  "https://wbvb.nl/themes/wp-rijkshuisstijl" );
-define( 'CHILD_THEME_VERSION',              "0.9.8" );
-define( 'CHILD_THEME_VERSION_DESCRIPTION',  "CSS voor onderwerppagina." );
+define( 'CHILD_THEME_VERSION',              "0.10.1" );
+define( 'CHILD_THEME_VERSION_DESCRIPTION',  "Div CSS styling issues: h2, external link." );
 define( 'SHOW_CSS_DEBUG',                   false );
 
 if ( SHOW_CSS_DEBUG && WP_DEBUG ) {
